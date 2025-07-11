0d40fbf1e14c188cd2cae37d30ab411be09ef8d9
activemedia12 <activemediaprint@gmail.com>
2025-07-11 16:48:11 +0800

    Fix Bugs and on Update Dashboard

--- pages/product_info.php
+++ pages/product_info.php
@@ -105,7 +105,6 @@ $delivery_history = $delivery_stmt->get_result();
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <link rel="stylesheet" href="../assets/css/style.css">
     <style>
         @keyframes centerZoomIn {
             0% {
